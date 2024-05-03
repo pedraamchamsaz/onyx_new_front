157cb3ebc20711a0b5fc4c71c50fb5f0ca1f63f5
pedraamchamsaz <151649370+pedraamchamsaz@users.noreply.github.com>
2024-05-03 11:56:43 +0100

    Pre-ShadCN install

--- app/page.js
+++ app/page.js
@@ -1,15 +1,25 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Footer from "@/components/Footer";
+import { titan_one } from "@/constants/fonts";
+import CustomFilters from "@/components/CustomFilters";
 
 export default function Home() {
   return (
     <main className="">
       <Navbar />
+
       <Hero />
+
       <div id='events' className="h-screen bg-white">
-        
+        <div className="px-6 sm:px-16 py-10">
+          <h1 className={`${titan_one.className} text-5xl font-bold`}>Event Catalogue</h1>
+          <p className="mt-2">Explore events here</p>
+        </div>
+
+        <CustomFilters />
       </div>
+
       <Footer />
     </main>
   );
---
+++ components/CustomFilters.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CustomFilters = () => {
+  return (
+    <div className='px-6 sm:px-16 border-t'>
+        CustomFilters
+    </div>
+  )
+}
+
+export default CustomFilters
