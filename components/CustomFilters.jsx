f684c3e414edaf1d76190f8843fb0b9822c4d3f5
pedraamchamsaz <151649370+pedraamchamsaz@users.noreply.github.com>
2024-05-03 17:21:45 +0100

    Saving progress

--- components/CustomFilters.jsx
+++ components/CustomFilters.jsx
@@ -18,7 +18,7 @@ const CustomFilters = () => {
                     {filters.map((filter) => (
                         <div key={filter.name} className="flex flex-col items-center hover:border-b-4 hover:border-b-gray-400 border-b-4 border-b-black/0 text-gray-500 hover:text-black transition">
                             <span className="material-symbols-outlined">{filter.image}</span>
-                            <p className="text-[10px] text-center mt-2">{filter.name}</p>
+                            <p className="text-[10px] text-center my-2">{filter.name}</p>
                         </div>
                     ))}
                 </CarouselItem>
